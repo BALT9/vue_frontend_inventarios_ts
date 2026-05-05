39b37de485e1cf8693af50636e4d227bca66b676
willy <willybalt.2017@gmail.com>
2026-05-05 12:04:21 -0400

    components prime vue

--- src/views/admin/users/Users.vue
+++ src/views/admin/users/Users.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue';
 import usersService from '../../../services/users.service';
 import type { UserInterface } from '../../../types/UserInterface';
-import { Button, InputText } from 'primevue';
+import { Button, Column, DataTable, Dialog, InputText } from 'primevue';
 
 const users = ref<UserInterface[]>([]);
 
@@ -13,6 +13,9 @@ const userDataBlank = {
 }
 
 const user = ref<UserInterface>({ ...userDataBlank });
+
+// Dialog 
+const visible = ref<boolean>(false);
 
 async function listarUsers() {
     const { data } = await usersService.index();
@@ -39,6 +42,7 @@ async function crearUsers() {
 
             listarUsers();
             user.value = { ...userDataBlank };
+            visible.value = false;
 
         } else {
             const { data } = await usersService.store(user.value);
@@ -46,6 +50,7 @@ async function crearUsers() {
 
             listarUsers();
             user.value = { ...userDataBlank };
+            visible.value = false;
         }
     } catch (error) {
         console.log(error);
@@ -54,6 +59,7 @@ async function crearUsers() {
 }
 
 async function funEditarUser(dataUser: UserInterface) {
+    visible.value = true;
     console.log(JSON.stringify(dataUser, null, 2));
     user.value = dataUser;
 }
@@ -73,13 +79,13 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="max-w-6xl mx-auto p-6 space-y-10">
+    <div class="max-w-6xl mx-auto p-6 space-y-10 bg-white">
 
         <!-- TITLE -->
         <h1 class="text-3xl font-bold text-color">Users</h1>
 
         <!-- FORM CARD -->
-        <div class="bg-surface-card shadow-lg rounded-2xl p-6">
+        <!-- <div class="bg-surface-card shadow-lg rounded-2xl p-6">
 
             <form class="grid grid-cols-1 md:grid-cols-2 gap-5">
 
@@ -104,19 +110,44 @@ onMounted(() => {
                 </div>
 
             </form>
-        </div>
+        </div> -->
 
-        <!-- DEBUG (opcional) -->
-        <div class="bg-surface-100 p-4 rounded-xl text-xs overflow-auto">
-            <pre>{{ user }}</pre>
+        <div class="card flex justify-center">
+            <Button label="Crear Usuario" @click="visible = true" />
+            <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }">
+                <span class="text-surface-500 dark:text-surface-400 block mb-8">Update your information.</span>
+                <div class="flex flex-col gap-2 md:col-span-2">
+                    <label class="text-sm font-medium text-color">Ingrese Nombre</label>
+                    <InputText class="w-full" v-model="user.username" />
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <label class="text-sm font-medium text-color">Ingrese Correo</label>
+                    <InputText type="email" class="w-full" v-model="user.email" />
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <label class="text-sm font-medium text-color">Ingrese Contraseña</label>
+                    <InputText type="password" class="w-full" v-model="user.password" />
+                </div>
+
+                <div class="md:col-span-2">
+                    <Button type="button" label="Guardar Usuario" icon="pi pi-save" class="w-full"
+                        @click="crearUsers()" />
+                </div>
+
+                <!-- DEBUG (opcional) -->
+                <div class="bg-surface-100 p-4 rounded-xl text-xs overflow-auto">
+                    <pre>{{ user }}</pre>
+                </div>
+            </Dialog>
         </div>
 
         <!-- TABLE CARD -->
         <div class="bg-surface-card shadow-lg rounded-2xl overflow-hidden">
 
-            <table class="w-full text-sm">
+            <!-- <table class="w-full text-sm">
 
-                <!-- HEADER -->
                 <thead class="bg-surface-100 text-color text-xs uppercase">
                     <tr>
                         <th class="p-4 text-left">ID</th>
@@ -125,8 +156,6 @@ onMounted(() => {
                         <th class="p-4 text-center">Acciones</th>
                     </tr>
                 </thead>
-
-                <!-- BODY -->
                 <tbody class="divide-y divide-surface-border">
 
                     <tr v-for="u in users" :key="u.id" class="hover:bg-surface-50 transition">
@@ -152,7 +181,21 @@ onMounted(() => {
                     </tr>
 
                 </tbody>
-            </table>
+            </table> -->
+
+            <DataTable :value="users" showGridlines tableStyle="min-width: 50rem">
+                <Column field="id" header="Code"></Column>
+                <Column field="username" header="Name"></Column>
+                <Column field="email" header="Email"></Column>
+                <Column :exportable="false" style="min-width: 12rem" header="Acciones">
+                    <template #body="slotProps">
+                        <Button icon="pi pi-pencil" variant="outlined" rounded class="mr-2"
+                            @click="funEditarUser(slotProps.data)" />
+                        <Button icon="pi pi-trash" variant="outlined" rounded severity="danger"
+                            @click="funEliminarUser(slotProps.data)" />
+                    </template>
+                </Column>
+            </DataTable>
         </div>
 
         <!-- RAW JSON (debug opcional) -->
