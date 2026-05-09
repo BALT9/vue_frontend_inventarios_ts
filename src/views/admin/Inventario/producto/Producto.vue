69c7d12eeb991cbca00b372eeb01d961ed75e0a1
willy <willybalt.2017@gmail.com>
2026-05-09 17:15:51 -0400

    exportacion de productos en un excel

--- src/views/admin/Inventario/producto/Producto.vue
+++ src/views/admin/Inventario/producto/Producto.vue
@@ -3,7 +3,7 @@
 import { onMounted, ref } from 'vue';
 import productoService from '../../../../services/producto.service';
 import type { ProductoInterface } from '../../../../types/ProductoInterface';
-import { Button, Column, DataTable, Dialog, InputText } from 'primevue';
+import { Button, Column, DataTable, Dialog, FileUpload, InputText, Toolbar } from 'primevue';
 
 
 const productos = ref<ProductoInterface[]>([]);
@@ -12,6 +12,8 @@ const visible = ref<boolean>(false);
 
 const cargando = ref<boolean>(true);
 const totalRecords = ref<number>(0);
+
+const dt = ref();
 
 const lazyParams = ref({
     page: 0,
@@ -34,6 +36,10 @@ const onPage = (event: any) => {
     lazyParams.value = event;
     listarProductos();
 }
+
+const exportCSV = (event: any) => {
+    dt.value.exportCSV();
+};
 
 
 </script>
@@ -62,13 +68,27 @@ const onPage = (event: any) => {
     </div>
 
     <div class="card">
-        <DataTable :value="productos" paginator :rows="10" @page="onPage($event)" lazy :loading="cargando"
-            :totalRecords="totalRecords" :rowsPerPageOptions="[1, 2, 5, 10, 20, 50]" tableStyle="min-width: 50rem">
-            <Column field="nombre" header="Name" style="width: 25%"></Column>
-            <Column field="descripcion" header="Country" style="width: 25%"></Column>
-            <Column field="precio_venta_actual" header="Company" style="width: 25%"></Column>
-            <Column field="marca" header="Marca" style="width: 25%"></Column>
-        </DataTable>
+        <Toolbar class="mb-6">
+            <template #start>
+                <Button label="New" icon="pi pi-plus" class="mr-2" @click="" />
+            </template>
+
+            <template #end>
+                <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" customUpload
+                    chooseLabel="Import" class="mr-2" auto :chooseButtonProps="{ severity: 'secondary' }" />
+                <Button label="Export" icon="pi pi-upload" severity="secondary" @click="exportCSV($event)" />
+            </template>
+        </Toolbar>
+
+        <div class="card">
+            <DataTable ref="dt" :value="productos" paginator :rows="10" @page="onPage($event)" lazy :loading="cargando"
+                :totalRecords="totalRecords" :rowsPerPageOptions="[1, 2, 5, 10, 20, 50]" tableStyle="min-width: 50rem">
+                <Column field="nombre" header="Name" style="width: 25%"></Column>
+                <Column field="descripcion" header="Country" style="width: 25%"></Column>
+                <Column field="precio_venta_actual" header="Company" style="width: 25%"></Column>
+                <Column field="marca" header="Marca" style="width: 25%"></Column>
+            </DataTable>
+        </div>
     </div>
 
 
